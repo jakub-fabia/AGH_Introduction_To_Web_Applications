dd3de5279c9e050e35f509b04a5b15562309c26b
jakub-fabia <jakub.fabia@proton.me>
2024-11-13 17:58:17 +0100

    Done

--- Project1/js/main.js
+++ Project1/js/main.js
@@ -5,3 +5,16 @@ function toggleLanguage(element) {
         element.textContent = "EN";
     }
 }
+
+function onClickImage(element) {
+    document.getElementById("fullscreen-video").style.display = "none";
+    document.getElementById("fullscreen-image").style.display = "block";
+    document.getElementById("fullscreen-image").src = element.src;
+    document.getElementById("fullscreen").style.display = "flex";
+}
+function onClickVideo(element) {
+    document.getElementById("fullscreen-video").style.display = "block";
+    document.getElementById("fullscreen-image").style.display = "none";
+    document.getElementById("fullscreen-video").src = element.src;
+    document.getElementById("fullscreen").style.display = "flex";
+}
